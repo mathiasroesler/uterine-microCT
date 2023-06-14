79cbcd2bbaf907ed12e1ec551cc7ee58b1d39083
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-06-15 10:49:59 +1200

    Changed the size of the moving average window

--- code/thickness_analysis/s_histology-analysis.py
+++ code/thickness_analysis/s_histology-analysis.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
 	mask_stack = utils.loadImageStack(os.path.join(
 		full_path, "{}".format(horn)), extension=args.extension)
 
-	circular_win_size = round(0.10 * args.points)
+	circular_win_size = round(0.05 * args.points)
 
 	print("   Finding centreline")
 	centreline_dict = scipy.io.loadmat(full_path + 
