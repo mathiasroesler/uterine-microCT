66590ac3a17e2dc3605de992a42c4a97bc6225fb
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-06-14 14:20:27 +1200

    Included an if statement to make skip the rotation if the slice is already aligned with the z-axis

--- code/thickness_analysis/rotateImageStack.m
+++ code/thickness_analysis/rotateImageStack.m
@@ -9,7 +9,7 @@ function rotated_stack = rotateImageStack(img_stack, region, nb_used_slices)
 %    vector.
 %
 %   Return:
-%    - rotated_stack, stack of N MxP rotated images, 
+%    - rotated_stack, stack of N MxP rotated images,
 %   rotated_stack(MxPxN).
 nb_slices = size(img_stack, 3);
 rotated_stack = zeros(size(img_stack));
@@ -33,10 +33,10 @@ for k = 1:nb_slices
 
     % Next mask for finding rotation axis
     if k < nb_slices-nb_used_slices
-        next_mask = img_stack(:, :, k+nb_used_slices); 
+        next_mask = img_stack(:, :, k+nb_used_slices);
     else
         % Use the previous slices to get rotation vector
-        next_mask = img_stack(:, :, k-nb_used_slices); 
+        next_mask = img_stack(:, :, k-nb_used_slices);
     end
     % Find centre points
     cur_centrepoints = findCentrepoints(cur_mask, region);
@@ -56,43 +56,48 @@ for k = 1:nb_slices
     centre_vector = [next_centrepoints - cur_centrepoints, nb_used_slices];
     centre_vector = centre_vector ./ norm(centre_vector);
 
-    % Create the transformation matrix
-    origin = [cur_centrepoints, 0];
-    T = findRotationMatrix(centre_vector, [0, 0, 1], origin);
-
-    % Pad the current mask to make a 3D object for rotation
-    mask_3D = padarray(cur_mask, [0, 0, 1], 0, 'post');
-    
-    % Rotate image and collapse it onto the XY plane
-    rotated_mask = sum(imwarp(mask_3D, affine3d(T)), 3);
-
-    % Resize the rotated image to have dimensions of original
-    dim_diff = size(cur_mask) - size(rotated_mask);
-    
-    if dim_diff(1) > 0
-        rotated_mask = padarray(rotated_mask, [dim_diff(1), 0], 'post');
+    if isequal(centre_vector, [0, 0, 1])
+        rotated_stack(:, :, k) = imclose(cur_mask, strel("disk", 1, 4));
 
     else
-        rotated_mask = rotated_mask(1:end+dim_diff(1), :);
-    end
+        % Create the transformation matrix
+        origin = [cur_centrepoints, 0];
+        T = findRotationMatrix(centre_vector, [0, 0, 1], origin);
 
-    if dim_diff(2) > 0
-        rotated_mask = padarray(rotated_mask, [0, dim_diff(2)], 'post');
+        % Pad the current mask to make a 3D object for rotation
+        mask_3D = padarray(cur_mask, [0, 0, 1], 0, 'post');
 
-    else
-        rotated_mask = rotated_mask(:, 1:end+dim_diff(2));
-    end   
+        % Rotate image and collapse it onto the XY plane
+        rotated_mask = sum(imwarp(mask_3D, affine3d(T)), 3);
 
-    % Check the ratio of white pixel to remove ill-rotated slices
-    nb_w_pixels = sum(sum(rotated_mask > 0));
+        % Resize the rotated image to have dimensions of original
+        dim_diff = size(cur_mask) - size(rotated_mask);
 
-    if nb_w_pixels / numel(rotated_mask) > 1e-3
-        % Clean up mask and add rotated mask to stack
-        rotated_stack(:, :, k) = imclose(rotated_mask, strel("disk", 1, 4));
+        if dim_diff(1) > 0
+            rotated_mask = padarray(rotated_mask, [dim_diff(1), 0], 'post');
 
-    else
-        disp("Slice " + k + ": bad rotation");
-        idx_to_remove = [idx_to_remove, k];
+        else
+            rotated_mask = rotated_mask(1:end+dim_diff(1), :);
+        end
+
+        if dim_diff(2) > 0
+            rotated_mask = padarray(rotated_mask, [0, dim_diff(2)], 'post');
+
+        else
+            rotated_mask = rotated_mask(:, 1:end+dim_diff(2));
+        end
+
+        % Check the ratio of white pixel to remove ill-rotated slices
+        nb_w_pixels = sum(sum(rotated_mask > 0));
+
+        if nb_w_pixels / numel(rotated_mask) > 1e-3
+            % Clean up mask and add rotated mask to stack
+            rotated_stack(:, :, k) = imclose(rotated_mask, strel("disk", 1, 4));
+
+        else
+            disp("Slice " + k + ": bad rotation");
+            idx_to_remove = [idx_to_remove, k];
+        end
     end
 end
 
