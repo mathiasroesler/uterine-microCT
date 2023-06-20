e2ce2fd9f2eb0d31ffcd9f354502b8ed0c54a08e
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-06-20 16:04:48 +1200

    Wrote a function to extract the fibre angles using the centreline to find the correct orientation plane

---
+++ code/structure_tensor/ComputeFibreAngle.m
@@ -0,0 +1,51 @@
+function angle = ComputeFibreAngle(fibre, centrepoints, cur_X, cur_Z)
+%COMPUTEFIBREANGLE Computes the angle in degrees of the fibre relative to
+%the plane given by the vector between two centre points. 
+%
+%   Inputs:
+%    - fibre, 3D array containing the XYZ coordinates of the fibre.
+%    - centrepoints, 6xN, list of centrepoints.
+%    - cur_X, x coordinate of the current fibre in the general coordinate
+%    system.
+%    - cur_Z, z coordinate of the current fibre in the general coordinate
+%    system.
+%
+%   Return:
+%    - angle, angle between the fibre and the current plane in degrees.
+if isempty(centrepoints)
+    z_vector = [0; 0; 1];
+
+else
+    if all(centrepoints(3:4, cur_Z))
+        % If a middle point is found
+        % Define the z vector as the centre vector between current slice
+        % and the current + 5th slice.
+        cur_idx = 3;
+        cur_centrepoint = centrepoints(cur_idx:cur_idx+1, cur_Z);
+
+    elseif ~all(centrepoints(5:6, cur_Z))
+        % If there is only one centre point found
+                cur_idx = 5;
+        cur_centrepoint = centrepoints(cur_idx:cur_idx+1, cur_Z);
+
+    else
+        % If there are left and right centre points find the neares to
+        % cur_X
+        [~, cur_idx] = min(abs(centrepoints(1:2:end, cur_Z) - cur_X));
+        cur_centrepoint = centrepoints(cur_idx:cur_idx+1, cur_Z);
+
+    end
+
+    if cur_Z + 5 > size(centrepoints, 2)
+        % If there are not enough slices use the previous slices
+        % instead
+        next_centrepoint = centrepoints(cur_idx:cur_idx+1, cur_Z - 5);
+        z_vector = cur_centrepoint - next_centrepoint;
+    else
+        next_centrepoint = centrepoints(cur_idx:cur_idx+1, cur_Z + 5);
+        z_vector = next_centrepoint - cur_centrepoint;
+    end
+end
+
+angle = rad2deg(acos(fibre * z_vector));
+end
