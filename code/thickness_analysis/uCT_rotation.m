d59af97f9b56a475f63e69750e36b73a5a1ef662
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-06-21 10:17:27 +1200

    Updated to used the TOML format and single parameter file

--- code/thickness_analysis/uCT_rotation.m
+++ code/thickness_analysis/uCT_rotation.m
@@ -1,28 +1,54 @@
-dir_name = "AWA015_PTA_1_Rec_Trans/downsampled/muscle_segmentation/";
-extension = "png";
-regions = ["left", "right"];
-nb_used_slices = 10;
-base_dir = join([getenv("HOME"), "Documents/phd/microCT/data", dir_name], '/');
+function uCTRotation(dir_path, base_name, regions, nb_used_slices, ...
+    downsampled, extension)
+%UCTCENTRELINE Computes the centreline for the dataset provided by
+%base_name given the selected regions.
+%   
+%   base_dir is $HOME/Documents/phd/
+%
+%   Input:
+%    - dir_path, path to the directory containing the dataset from base_dir
+%    - base_name, name of the dataset.
+%    - region, either left, right or both, used to sort the centrepoints. 
+%    - nb_used_slices, number of slices to use to determine centre vector,
+%    default value 5.
+%    - downsampled, true if the dataset has been downsampled, default value
+%    is true.
+%    - extension, extension of the images to load, default value is png.
+%
+%   Return: 
+if nargin < 6
+    extension = "png";
+end
+if nargin < 5
+    downsampled = true;
+end
+if nargin < 4
+    nb_used_slices = 5;
+end
 
-toml_map = toml.read(base_dir + "analysis.toml");
+load_directory = join([getenv("HOME"), "Documents/phd/", dir_path, base_name], '/');
+
+% Load parameters
+toml_map = toml.read(join([load_directory, base_name + ".toml"], '/'));
 params = toml.map_to_struct(toml_map);
+start_nb = params.thickness.start_nb;
 
-mask_paths = getImagePaths(base_dir, extension);
+if downsampled
+    % Deal with downsampled dataset
+    load_directory = join([load_directory, "downsampled"], '/');
+end
+
+mask_paths = getImagePaths(load_directory, extension);
 mask_stack = loadImageStack(mask_paths);
 
 for k = 1:length(regions)
     region = regions(k);
 
     if strcmp(region, "left")
-        start_nb = params.left.start_nb;
-        end_nb = params.left.end_nb;
+        end_nb = params.thickness.left.end_nb;
     elseif strcmp(region, "right")
-        start_nb = params.right.start_nb;
-        end_nb = params.right.end_nb;
-    elseif strcmp(region, "body")
-        start_nb = params.body.start_nb;
-        end_nb = params.body.end_nb;        
-    else
+        end_nb = params.thickness.right.end_nb;
+
         error("Error: invalid horn selection.");
     end
 
@@ -31,7 +57,7 @@ for k = 1:length(regions)
         mask_stack(:, :, start_nb:end_nb), region, nb_used_slices); 
 
     disp("Saving region: " + region);
-    saveImageStack(rotated_stack, base_dir + region, ...
+    saveImageStack(rotated_stack, load_directory + region, ...
         params.prefix, start_nb, extension);
 
     clear rotated_stack % Save memory
