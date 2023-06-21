aa3f7c3befbedceee1eca514d4184f012ed39d27
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-06-21 14:11:34 +1200

    Fixed the projection fig generator script and converted it to the new convention at the same time

--- code/scripts/projection_fig_gen.py
+++ code/scripts/projection_fig_gen.py
@@ -7,6 +7,7 @@
 
 import os
 import argparse
+import scipy.io
 import numpy as np
 import utils.utils as utils
 import matplotlib.pyplot as plt
@@ -18,8 +19,10 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description=
 		"Specific script to generate the projection point plot")
 
-	parser.add_argument("input_folder", type=str, metavar="input-folder",
-		help="name of the folder containing the images")
+	parser.add_argument("dir_path", type=str, metavar="dir-path",
+		help="path from BASE to the dataset")
+	parser.add_argument("base_name", type=str, metavar="base-name",
+		help="name of the dataset")
 	parser.add_argument("img_nb", type=str, metavar="img-nb", 
 		help="number of the image to use")
 	parser.add_argument("-e", "--extension", type=str, metavar="extension",
@@ -28,22 +31,41 @@ if __name__ == "__main__":
 		help="horn to process", default="right")
 	parser.add_argument("-p", "--points", type=int, metavar="points",
 		help="number of points to use for the projection", default=8)
+	parser.add_argument("--not-d", action='store_true',
+		help="flag used if the dataset is not downsampled")
 
 	# Parse input arguments
 	args = parser.parse_args()
 	
-	full_path = os.path.join(utils.HOME, utils.BASE, args.input_folder, 
-		utils.MASK_FOLDER)
+	load_directory = os.path.join(utils.HOME, utils.BASE, args.dir_path,
+		args.base_name)
 
-	param_file = full_path + "/analysis.toml"
+	# Get parameters
+	param_file = os.path.join(load_directory, args.base_name + ".toml")
 	params = utils.parseTOML(param_file)
 
-	img_file = full_path + "/{}_horn/".format(args.horn) + params["prefix"] + args.img_nb + "." + args.extension
-	img = plt.imread(img_file)
+	if not args.not_d:
+		# If the dataset is downsampled
+		load_directory = os.path.join(load_directory, "downsampled")
 
-	# 3D array needed for it to work
-	centreline = projection.findCenterline(np.array([img, img]), 
-		horn=args.horn)
-	projection_points = projection.findProjectionPoints(img, centreline[0], args.points)
+	# Add the muscle segmentation to the load directory
+	load_directory = os.path.join(load_directory, "muscle_segmentation", 
+		args.horn)
 
-	plots.plotProjectionPoints(img, centreline[0], projection_points)
+	# Load the centreline
+	centreline_dict = scipy.io.loadmat(load_directory + "/centreline.mat")
+	centreline = np.transpose(centreline_dict["centreline"])
+	centreline = np.round(centreline).astype(int) # Round and convert to int
+
+	# Image to use for projection
+	img_name = os.path.join(load_directory, 
+		params["prefix"] + '_' + args.img_nb + '.' + args.extension)
+	img = plt.imread(img_name) # Load image
+
+	# Rectify image number because index starts at 0
+	projection_points = projection.findProjectionPoints(img, 
+		centreline[int(args.img_nb)-1], args.points, args.horn)
+
+	# Plot everything
+	plots.plotProjectionPoints(img, centreline[int(args.img_nb)-1], 
+		projection_points)
