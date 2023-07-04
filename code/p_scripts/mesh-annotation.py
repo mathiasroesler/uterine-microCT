f58d2056f596c9fad129e41762b8eda8e1fb1f1c
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-07-04 13:11:33 +1200

    Changed the annotation script to align the annotation with the centre vectors

--- code/p_scripts/mesh-annotation.py
+++ code/p_scripts/mesh-annotation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# mesh-annotation.py: Script that adds the thickness data to a vtu mesh
+# mesh-annotation.py: Script that adds the thickness data to a vtu/vtk mesh
 # Author: Mathias Roesler
 # Last modified: 06/23
 
 import os
 import sys
 import meshio
+import scipy.io
 import argparse
 import numpy as np
 import utils.utils as utils
@@ -23,14 +24,15 @@ if __name__ == "__main__":
 		help="path from BASE to the mesh, default mesh/")
 	parser.add_argument("--data-dir", type=str, default="microCT/data",
 		help="path form BASE to the thickness data, default microCT/data")
-	parser.add_argument("--horn", type=str, choices={"left", "right", "both"},
-		help="horn to annotate, default both", default="both")
+	parser.add_argument("-s", "--switch", action='store_true',
+		help="switches the labels of the left and right horn, default False")
 	parser.add_argument("--not-d", action='store_true',
 		help="flag used if the dataset is not downsampled, default False")
 	parser.add_argument("-e", "--extension", choices={"vtu", "vtk"},
 		help="mesh extesion, default value vtk", default="vtk")
 
 	args = parser.parse_args()
+	nb_used_slices = 5
 
 	# Set arguments
 	mesh_directory = os.path.join(utils.HOME, utils.BASE, args.mesh_dir)
@@ -46,84 +48,95 @@ if __name__ == "__main__":
 	# Add the muscle segmentation to the load directory
 	thickness_directory = os.path.join(thickness_directory, 
 		"muscle_segmentation")
-	horns = ["left", "right"]
+	horns = ["right", "left"]
 	
+	# Get the centreline
+	centreline_dict = scipy.io.loadmat(thickness_directory + "/centreline.mat")
+	centreline = np.transpose(centreline_dict["centreline"])
+	centreline = np.round(centreline).astype(int) # Round and convert to int
+
 	# Read the mesh file
 	print("Loading mesh {}".format(mesh_name + '.' + args.extension))
 	mesh = meshio.read(mesh_name + "." + args.extension)
-	x_coords = mesh.points[:, 1]
-	y_coords = mesh.points[:, 0]
-	z_coords = mesh.points[:, 2]
+	nb_points = len(mesh.points)
 
 	# Re-centre z coordinates of the mesh with origin
-	z_coords = z_coords - min(z_coords)
-	
-	nb_points = len(z_coords)
-	nb_slices = round(z_coords.max())
-	
+	mesh.points[:, 2] = mesh.points[:, 2] - min(mesh.points[:, 2])
+	mesh.points[:, 0] = -mesh.points[:, 0]
+	mesh.points[:, 1] = -mesh.points[:, 1]
+
 	# Read thickness data
 	thickness_data = np.load(thickness_directory + "/muscle_thickness.pkl",
 		allow_pickle=True)
-	left_thickness = thickness_data['left']
-	right_thickness = thickness_data['right']
 
 	# Thickness data dictionary to be added to the mesh 	
 	point_data_dict = dict()
 	point_data_array = np.zeros((nb_points, 1))
 	point_data_name = "thickness"
-	
-	print("Annotating mesh")
-	for i in range(nb_slices):
-		# Get the indices of the points on the slice
-		slice_idx_list = np.where((z_coords >= i) * (z_coords < (i+1)))[0]
-		
-		# Deal with edge cases
-		if i == 0:
-			slice_idx_list = np.where((z_coords >= (i-1)) *
-				(z_coords < (i+1)))[0]
-		
-		elif i == nb_slices-1:
-			slice_idx_list = np.where((z_coords >= i) *
-				(z_coords < (i+2)))[0]
-				
-		# Create the thickness array 
-		thickness_array = np.zeros((2, 1))
 
-		if i < len(left_thickness):
-			thickness_array[0] = left_thickness[i]
+	# Split the mesh elements into left and right
+	element_idx_dict = dict()
+	x_delta = mesh.points[:, 0].max() - mesh.points[:, 0].min() 
+	element_idx_dict['left'] = np.where(abs(mesh.points[:, 0]) < x_delta)[0]
+	element_idx_dict['right'] = np.where(abs(mesh.points[:, 0]) >= x_delta)[0]
 
-		if i < len(right_thickness):
-			thickness_array[1] = right_thickness[i]
-		
-		# Remove 0 values if the horns are not the same length
-		thickness_array = np.delete(thickness_array, 
-			np.where(thickness_array == 0)[0])
+	# Split the centre points into left and right
+	centrepoint_coords = dict()
+	centrepoint_coords['left'] = centreline[:, 0:2]
+	centrepoint_coords['right'] = centreline[:, 4:6]
 
-		# If two horns
-		if thickness_array.shape[0] == 2:
-			slice_y_points = y_coords[slice_idx_list]
+	for i, horn in enumerate(horns):
+		print("Annotating {} horn".format(horn))
+		if args.switch:
+			# If the centrepoints need to be switched
+			i = i-1
 
-			# Find the middle of the slice
-			slice_y_max = slice_y_points.max()
-			slice_y_min = slice_y_points.min()
-			mid_point = (slice_y_max + slice_y_min) / 2
+		thickness = thickness_data[horn]
+		element_idx = element_idx_dict[horns[i]]
+		elements = mesh.points[element_idx]
+		centrepoints = centrepoint_coords[horns[i]]
 
-			left_idx_list = np.where((slice_y_points <= mid_point))[0]
-			right_idx_list = np.where((slice_y_points > mid_point))[0]
+		for k in range(len(thickness)):
+			# Find the centre vector
+			if k >= len(thickness) - nb_used_slices:
+				# Use less slices to get centre vector
+				next_centrepoint = centrepoints[len(thickness)-1]
+				z_centre = len(thickness) - k
+			else:
+				next_centrepoint = centrepoints[k+nb_used_slices]
+				z_centre = nb_used_slices
+			
+			centre_vector = next_centrepoint - centrepoints[k]
 
-			point_data_array[slice_idx_list[left_idx_list]] = thickness_array[0]
-			point_data_array[slice_idx_list[right_idx_list]] = thickness_array[1]
+			# Add z component
+			centre_vector = np.append(centre_vector, z_centre)
 
-		# If only one horn left
-		elif thickness_array.shape[0] == 1:
-			point_data_array[slice_idx_list] = thickness_array[0]
+			# Ensure that the direction of centre vector is consistent
+			condition_1 = centre_vector[0] < 0 and horns[i] == "right"
+			condition_2 = centre_vector[0] > 0 and horns[i] == "left"
+
+			if condition_1 or condition_2:
+				centre_vector[0] = -centre_vector[0]
+
+			# Slice plane normal vector and origin point
+			plane_normal = centre_vector / np.linalg.norm(centre_vector)
+			plane_origin = np.array([centrepoints[k, 0], centrepoints[k, 1], k])
+
+			if k == 0:
+				# Ensure that first centre vector is aligned with z
+				plane_normal = np.array([0, 0, 1])
+
+			norms = np.dot(elements-plane_origin, plane_normal)
+			idx_list = element_idx[(norms <= 10) & (norms >= 0)]
+			point_data_array[idx_list] = thickness[k]
 
 	# Add the data dictionary to the mesh
 	point_data_dict[point_data_name] = point_data_array
 	mesh.point_data = point_data_dict
-	mesh.points[:, 1] = x_coords
-	mesh.points[:, 0] = y_coords
-	mesh.points[:, 2] = z_coords
+
+	# Flip mesh back to original position
+	mesh.points[:, 0] = -mesh.points[:, 0]
+	mesh.points[:, 1] = -mesh.points[:, 1]
 
 	# Save new mesh
 	print("Saving mesh {}".format(mesh_name + "_annotated." + args.extension))
