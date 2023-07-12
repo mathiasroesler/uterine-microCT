5593a2a8f6a273ce72ca44d6af0976fc6ce1b40b
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-07-12 15:30:54 +1200

    Added the possibility to crop the images when loading

--- code/utils/loadImageStack.m
+++ code/utils/loadImageStack.m
@@ -1,17 +1,28 @@
-function img_stack = loadImageStack(img_paths, mask_paths)
+function img_stack = loadImageStack(img_paths, mask_paths, xlim, ylim)
 %LOADIMAGESTACK Loads the images contains in the img_paths cell array.
 %
-% If mask_paths is specified, the masks are applied to the images.
+% If mask_paths is specified, the masks are applied to the images. The
+% images are cropped by xlim and ylim.
 %   
 %   Input:
 %    - img_paths, cell array containing the path to every image,
 %    img_paths(N x 1).
 %    - mask_paths, cell array containing the path to every mask,
 %    mask_paths(N x 1), default value [].
+%    - xlim, array giving start and end values of x axis crop, default 
+%    value [].
+%    - ylim, array giving start and end values of y axis crop, default
+%    value [].
 %
 %   Return:
 %    - img_stack, matrix containing the images read from the paths. If
 %    mask_paths is specified, the masks are applied before return.
+if nargin < 4
+    ylim = [];
+end
+if nargin < 3
+    xlim = [];
+end
 if nargin < 2
     mask_paths = [];
 end
@@ -22,7 +33,22 @@ if nb_imgs ~= length(mask_paths) && ~isempty(mask_paths)
 end
 
 img = imread(img_paths{1}); % Read first image for size
-img_stack = zeros(size(img, 1), size(img, 2), nb_imgs, 'uint8'); 
+
+if isempty(xlim) && isempty(ylim)
+    % If no limits provided set x and y limits to be full image
+    xlim = [1, size(img, 1)];
+    ylim = [1, size(img, 2)];
+
+elseif ~isempty(xlim)
+    % If only x limits provided set y limits to be full image
+    ylim = [1, size(img, 2)];
+
+elseif ~isempty(ylim)
+    % If only y limits provided set x limits to be full image
+    xlim = [1, size(img, 1)];
+end
+
+img_stack = zeros(diff(xlim)+1, diff(ylim)+1, nb_imgs, 'uint8');
 
 if ~isempty(mask_paths)
     for k = 1:nb_imgs
@@ -35,12 +61,15 @@ if ~isempty(mask_paths)
         end
 
         mask = uint8(mask);
-        img_stack(:, :, k) = img .* mask;
+        img_stack(:, :, k) = img( ...
+            xlim(1):xlim(2), ylim(1):ylim(2)) .* mask( ...
+            xlim(1):xlim(2), ylim(1):ylim(2));
     end
 
 else
     for k = 1:nb_imgs
-        img_stack(:, :, k) = imread(img_paths{k});
+        img = imread(img_paths{k});
+        img_stack(:, :, k) = img(xlim(1):xlim(2), ylim(1):ylim(2));
     end
 
 end
