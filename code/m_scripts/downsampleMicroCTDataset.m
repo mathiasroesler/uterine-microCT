0a8700092e3fc0103711e7a45fb8beefe0a63aed
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-07-20 17:22:08 +1200

    Updated the downsampling function to only accept a resize factor

--- code/m_scripts/downsampleMicroCTDataset.m
+++ code/m_scripts/downsampleMicroCTDataset.m
@@ -1,4 +1,5 @@
-function downsampleMicroCTDataset(dir_path, base_name, varargin)
+function downsampleMicroCTDataset(dir_path, base_name, resize_factor, ...
+    varargin)
 %DOWNSAMPLEMICROCTDATASET Downsamples a uCT dataset. 
 %
 %   base_dir is $HOME/Documents/phd/ and set in utils/baseDir()
@@ -6,8 +7,7 @@ function downsampleMicroCTDataset(dir_path, base_name, varargin)
 %   Input:
 %    - dir_path, path to the directory containing the dataset from base_dir
 %    - base_name, name of the dataset.
-%    - new_resolution, either an array with the X, Y, and Z resolutions or
-%    the factor by which to downsample by.
+%    - resize_factor, the factor by which to downsample by, should be < 1.
 %    - batch_size, number of image to process in one batch, default 512.
 %    - img_extension, extension of the images in the uCT dataset, default
 %    bmp.
@@ -19,27 +19,25 @@ function downsampleMicroCTDataset(dir_path, base_name, varargin)
 % Modified by Mathias Roesler Nov 2022
 narginchk(3, 6);
 
-new_resolution = varargin{1};
-
 if nargin < 6
     save_extension = "png";
 
 else
-    save_extension = varargin{4};
+    save_extension = varargin{3};
 end
 
 if nargin < 5
     img_extension = "bmp";
 
 else
-    img_extension = varargin{3};
+    img_extension = varargin{2};
 end
 
 if nargin < 4
     batch_size = 512;
 
 else
-    batch_size = varargin{2};
+    batch_size = varargin{1};
 end
 
 % Directory where images are located
@@ -55,24 +53,14 @@ stack_size = params.end_nb - params.start_nb;
 
 log_file = join([save_directory, params.prefix + "_downsampled.log"], '/');
 
-if sum(size(new_resolution)) == 4
-    % The input is the new resolution vector with 3 components
-    new_nb_pixel_x = new_resolution(1);
-    new_nb_pixel_y = new_resolution(2);
-    new_stack_size = new_resolution(3);
-    z_factor = new_resolution(3);
-
-elseif isscalar(new_resolution)
-    % The input is the downsampling factory
-    new_nb_pixel_x = round(params.nb_pixel_x/new_resolution(1));
-    new_nb_pixel_y = round(params.nb_pixel_y/new_resolution(1));
-    new_stack_size = round(stack_size/new_resolution(1));
-    z_factor = new_resolution(1);
-
-else
-    error("The size of the input resolution is wrong. Size should be 1 or 3.")
+if ~isscalar(resize_factor)
+    error("The resize factor should be a scalar.")
 end
 
+% Estimate new resolution and number of pixels
+new_nb_pixel_x = round(params.nb_pixel_x*resize_factor);
+new_nb_pixel_y = round(params.nb_pixel_y*resize_factor);
+new_stack_size = round(stack_size*resize_factor);
 new_resolution_x = (params.nb_pixel_x/new_nb_pixel_x)*params.resolution; % um/pixel x-dir
 new_resolution_y = (params.nb_pixel_y/new_nb_pixel_y)*params.resolution; % um/pixel y-dir
 new_resolution_z = (stack_size/new_stack_size)*params.resolution; % um/pixel z-dir
@@ -93,15 +81,14 @@ for run = 1:nb_runs
     end
 
     batch_stack_size = last_image_nb - first_image_nb + 1;
-    new_batch_stack_size = round(batch_stack_size / z_factor);
+    new_batch_stack_size = round(batch_stack_size * resize_factor);
 
     % Load all images in current batch
     disp("Loading " + num2str(batch_stack_size) + " images in batch");
     img_stack = loadImageStack(img_paths(first_image_nb:last_image_nb));
 
     % Resize current batch stack
-    new_stack = imresize3(uint8(img_stack), [new_nb_pixel_x, ...
-        new_nb_pixel_y, new_batch_stack_size]); 
+    new_stack = imresize3(uint8(img_stack), resize_factor); 
 
     % Save current batch stack
     disp("Saving " + num2str(new_batch_stack_size) + " downsampled images");
