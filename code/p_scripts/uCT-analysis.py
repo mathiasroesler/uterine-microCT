325bff17d122b037252ff77dd4cd4acdb4a0a106
Mathias Roesler <mroe734@bn418243.uoa.auckland.ac.nz>
2023-09-11 10:26:39 +1200

    Changed the plotting of the angular thickness

--- code/p_scripts/uCT-analysis.py
+++ code/p_scripts/uCT-analysis.py
@@ -125,6 +125,12 @@ if __name__ == "__main__":
 
 	# Plot everything
 	plots.plotMuscleThickness(avg_thickness, errors)
-	plots.plotAngularThickness({"left": avg_slice_thickness["left"]})
-	plots.plotAngularThickness({"right": avg_slice_thickness["right"]})
+
+	# plots.plotAngularThickness({"left": avg_slice_thickness["left"]})
+	# plots.plotAngularThickness({"right": avg_slice_thickness["right"]})
+
+	plots.plotAngularThickness({"left": avg_slice_thickness["left"]}, 
+		projection=True)
+	plots.plotAngularThickness({"right": avg_slice_thickness["right"]}, 
+		projection=True)
 
